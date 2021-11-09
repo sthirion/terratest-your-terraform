d3773330cc19ade844d9dffca2d78b29a59d6c37
Sven Thirion <sven.thirion@slalom.com>
2021-11-09 13:54:38 -0600

    Simplifying test setup for demo

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,12 @@
 # ---------------------------------------------------------------------------------------------------------------------
 # DEPLOY A S3 BUCKET WITH VERSIONING ENABLED INCLUDING TAGS
-# See test/terraform_aws_s3_example_test.go for how to write automated tests for this code.
+# See terratest/s3_test.go for how to write automated tests for this code.
 # ---------------------------------------------------------------------------------------------------------------------
 
 data "aws_iam_policy_document" "s3_bucket_policy" {
   statement {
     effect = "Allow"
     principals {
-      # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-      # force an interpolation expression to be interpreted as a list by wrapping it
-      # in an extra set of list brackets. That form was supported for compatibility in
-      # v0.11, but is no longer supported in Terraform v0.12.
-      #
-      # If the expression in the following list itself returns a list, remove the
-      # brackets to avoid interpretation as a list of lists. If the expression
-      # returns a single list item then leave it as-is and remove this TODO comment.
       identifiers = [00000000]
       type        = "AWS"
     }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,21 +1,6 @@
 # Output variable definitions
 
-# output "arn" {
-#   description = "ARN of the bucket"
-#   value       = aws_s3_bucket.s3_bucket.arn
-# }
-
-# output "name" {
-#   description = "Name (id) of the bucket"
-#   value       = aws_s3_bucket.s3_bucket.id
-# }
-
 output "bucket_id" {
   description = "ID of the bucket"
   value       = aws_s3_bucket.test_bucket.id
-}
-
-output "bucket_arn" {
-  description = "ARN of the bucket"
-  value       = aws_s3_bucket.test_bucket.arn
 }
